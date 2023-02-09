cc5e8710a1fc231ed2f535569d1d60df1b1bffaf
Pipap <pipap443@entro.postbox.in.th>
2023-02-09 16:47:38 +0700

    Edit Initial

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-const express = require("express");
-const app = express();
-app.use(express.json())
-
-const route = require('./routes')
-route(app)
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`On port ${port}`))
+const express = require('express')
+const app = express()
+const route = require('./src/routes');
+route(app);
+app.listen(process.env.PORT || 3000)
---
+++ src/routes.js
@@ -0,0 +1,25 @@
+
+module.exports = (app) => {
+
+    app.get('/api/all', (req, res) => {
+        res.send(require('./res/get-all.json'));
+    });    
+    
+
+    app.get('/api/:keyName/:keyValue.(json)', (req, res) => {
+        res.send(require('./res/get-one.json'));
+    });   
+ 
+    app.post('/api/post', (req, res) => {
+        res.send(require('./res/post.json'));
+    });  
+ 
+    app.all('*', (req, res) => {
+        console.log('testttt')
+        res.send(require('./res/all.json'));
+    });
+
+    
+    
+}
+
